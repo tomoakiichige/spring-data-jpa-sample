0c50fe34dae5dcf59452deb25961fb5879efee8d
Tomoaki Ichige <ichigetomoaki1207@gmail.com>
2023-12-28 19:13:05 +0900

    init

---
+++ src/main/kotlin/com/example/springdatajpasample/SpringDataJpaSampleApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springdatajpasample
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataJpaSampleApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDataJpaSampleApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springdatajpasample/SpringDataJpaSampleApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springdatajpasample
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataJpaSampleApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
